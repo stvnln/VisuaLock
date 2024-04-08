c53f464152a211ecddec3a702590e02d3e6e2d91
stvnln <stevenlin4862@gmail.com>
2024-04-07 20:11:50 -0400

    Connected the image database to the password page

--- VisuaLock/app/build.gradle.kts
+++ VisuaLock/app/build.gradle.kts
@@ -50,5 +50,8 @@ dependencies {
     implementation("com.google.firebase:firebase-firestore:24.10.3")
     implementation ("com.google.android.gms:play-services-auth:21.0.0")
     implementation("com.google.firebase:firebase-auth:22.3.1")
+    implementation("com.github.bumptech.glide:glide:4.12.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.12.0")
+
 
 }
--- VisuaLock/app/src/main/java/com/example/visualock/PasswordActivity.java
+++ VisuaLock/app/src/main/java/com/example/visualock/PasswordActivity.java
@@ -2,6 +2,8 @@ package com.example.visualock;
 import java.util.*;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.util.Log;
 import android.widget.GridView;
 import android.content.Context;
 import android.view.View;
@@ -13,42 +15,47 @@ import android.widget.AdapterView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Patterns;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.firestore.DocumentSnapshot;
+
+public class PasswordActivity extends AppCompatActivity {
+    private FirebaseAuth auth;
+    private FirebaseFirestore firestore;
 
 
 
- public class PasswordActivity extends AppCompatActivity {
-     public static class ImageAdapter extends BaseAdapter {
-         private Context mContext;
-         private int[] mImageIds;
+    public static class ImageAdapter extends BaseAdapter {
+         private final Context mContext;
+         private final ArrayList<String> mImageUrls;
 
-         public ImageAdapter(Context context, int[] imageIds) {
+         public ImageAdapter(Context context, ArrayList<String> imageUrls) {
              mContext = context;
-             mImageIds = imageIds;
+             mImageUrls = imageUrls;
          }
 
          @Override
          public int getCount() {
-             return mImageIds.length;
+             return mImageUrls.size();
          }
 
          @Override
          public Object getItem(int position) {
-             return mImageIds[position];
+             return mImageUrls.get(position);
          }
 
          @Override
@@ -70,116 +77,138 @@ import com.google.firebase.firestore.FirebaseFirestore;
              }
 
              // Set the image resource for the ImageView
-             imageView.setImageResource(mImageIds[position]);
+             Glide.with(mContext).load(mImageUrls.get(position)).into(imageView);
              return imageView;
          }
 
      }
 
-     int[] colorImages = {
-             R.drawable.grey, R.drawable.pink, R.drawable.green, R.drawable.orange, R.drawable.yellow,
-             R.drawable.blue, R.drawable.black, R.drawable.red, R.drawable.purple
-     };
 
-     int[] treeImages = {
-             R.drawable.birch, R.drawable.cedar, R.drawable.maple, R.drawable.elm, R.drawable.willow, R.drawable.pine,
-             R.drawable.oak
-     };
-
-     int[] dailyObjectsImages = {
-             R.drawable.phone, R.drawable.carrot, R.drawable.mouse, R.drawable.balloon, R.drawable.boat,
-             R.drawable.glasses, R.drawable.table, R.drawable.keyboard, R.drawable.sun, R.drawable.book,
-             R.drawable.cloud, R.drawable.bottle, R.drawable.leaf, R.drawable.apple, R.drawable.plant,
-             R.drawable.key, R.drawable.mug, R.drawable.shoe
-     };
-
-     int[] animalImages = {
-             R.drawable.bear, R.drawable.rabbit, R.drawable.turtle, R.drawable.bird, R.drawable.giraffe, R.drawable.snake,
-             R.drawable.monkey, R.drawable.zebra, R.drawable.cat, R.drawable.koifish, R.drawable.fish, R.drawable.lion,
-             R.drawable.dog, R.drawable.squirrel, R.drawable.polar_bear, R.drawable.kangroo, R.drawable.frog, R.drawable.tiger
-     };
-
-     int[] placesImages = {
-             R.drawable.leaningtowerofpisa, R.drawable.colosseum, R.drawable.greatwallofchina, R.drawable.pyramidsofgiza,
-             R.drawable.christtheredeemer, R.drawable.neuschwansteincastle, R.drawable.stonehenge, R.drawable.acropolisofathens,
-             R.drawable.petronastowers, R.drawable.tajmahal, R.drawable.operahouse, R.drawable.machupicchu, R.drawable.eiffeltower,
-             R.drawable.goldengate, R.drawable.burjkhalifa
-     };
-
-     int[] vehicleImages = {
-             R.drawable.bike, R.drawable.firetruck, R.drawable.truck, R.drawable.sailboat, R.drawable.sportscar,
-             R.drawable.pickuptruck, R.drawable.cycle, R.drawable.cycle2, R.drawable.jetski, R.drawable.bus,
-             R.drawable.cargoship, R.drawable.helicopter, R.drawable.plane, R.drawable.mountrushmore, R.drawable.scooter,
-             R.drawable.ambullance, R.drawable.metro
-     };
-
-     protected void onCreate(Bundle savedInstanceState) {
-         super.onCreate(savedInstanceState);
-         setContentView(R.layout.activity_password_input);
-
-         ListView Tree = findViewById(R.id.cat_tree);
-         Tree.setAdapter(new ImageAdapter(this, treeImages));
-         ListView Color = findViewById(R.id.cat_color);
-         Color.setAdapter(new ImageAdapter(this, colorImages));
-         ListView dailyObjects = findViewById(R.id.cat_dailyObjects);
-         dailyObjects.setAdapter(new ImageAdapter(this,dailyObjectsImages));
-         ListView Animals = findViewById(R.id.cat_animals);
-         Animals.setAdapter(new ImageAdapter(this, animalImages));
-         ListView Places = findViewById(R.id.cat_places);
-         Places.setAdapter(new ImageAdapter(this, placesImages));
-         ListView Vehicle = findViewById(R.id.cat_vehicles);
-         Vehicle.setAdapter(new ImageAdapter(this,vehicleImages));
-
-         Tree.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-             @Override
-             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                 Toast.makeText(PasswordActivity.this, "Tree image clicked at position: " + position, Toast.LENGTH_SHORT).show();
+    ArrayList<String> colorImages;
+    ArrayList<String> treeImages;
+    ArrayList<String> dailyObjectsImages;
+    ArrayList<String> animalImages;
+    ArrayList<String> placesImages;
+    ArrayList<String> vehicleImages;
 
 
-             }
-         });
-         Color.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-             @Override
-             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                 Toast.makeText(PasswordActivity.this, "Color image clicked at position: " + position, Toast.LENGTH_SHORT).show();
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_password_input);
 
 
-             }
-         });
-         dailyObjects.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-             @Override
-             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                 Toast.makeText(PasswordActivity.this, "Daily Object image clicked at position: " + position, Toast.LENGTH_SHORT).show();
+
+        firestore = FirebaseFirestore.getInstance();
+        DocumentReference image_categories =  firestore.collection("image_categories").document("qPQROdVyhejjFHqYySWo");
+        image_categories.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot document) {
+                String documentID = document.getId();
+                Map<String, Object> data = document.getData();
+                Log.d("PasswordActivity", "Document ID is here: " + documentID);
+                if (data != null) {
+                    for (Map.Entry<String, Object> entry : data.entrySet()) {
+                        Log.d("PasswordActivity", "Document data is here: " + entry.getKey() + " = " + entry.getValue());
+                        String field = entry.getKey();
+                        ArrayList<String> value = (ArrayList<String>) entry.getValue();
+                        switch (field) {
+                            case "animalImages":
+                                animalImages = value;
+                                Log.d("PasswordActivity", "Animal Images: " + animalImages);
+                                break;
+                            case "colorImages":
+                                colorImages = value;
+                                Log.d("PasswordActivity", "Color Images: " + colorImages);
+                                break;
+                            case "dailyObjectsImages":
+                                dailyObjectsImages = value;
+                                Log.d("PasswordActivity", "Daily Objects Images: " + dailyObjectsImages);
+                                break;
+                            case "placesImages":
+                                placesImages = value;
+                                Log.d("PasswordActivity", "Places Images: " + placesImages);
+                                break;
+                            case "treeImages":
+                                treeImages = value;
+                                Log.d("PasswordActivity", "Tree Images: " + treeImages);
+                                break;
+                            case "vehicleImages":
+                                vehicleImages = value;
+                                Log.d("PasswordActivity", "Vehicle Images: " + vehicleImages);
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                }
+                ListView Tree = findViewById(R.id.cat_tree);
+                Tree.setAdapter(new ImageAdapter(PasswordActivity.this, treeImages));
+                ListView Color = findViewById(R.id.cat_color);
+                Color.setAdapter(new ImageAdapter(PasswordActivity.this, colorImages));
+                ListView dailyObjects = findViewById(R.id.cat_dailyObjects);
+                dailyObjects.setAdapter(new ImageAdapter(PasswordActivity.this,dailyObjectsImages));
+                ListView Animals = findViewById(R.id.cat_animals);
+                Animals.setAdapter(new ImageAdapter(PasswordActivity.this, animalImages));
+                ListView Places = findViewById(R.id.cat_places);
+                Places.setAdapter(new ImageAdapter(PasswordActivity.this, placesImages));
+                ListView Vehicle = findViewById(R.id.cat_vehicles);
+                Vehicle.setAdapter(new ImageAdapter(PasswordActivity.this,vehicleImages));
+
+                Tree.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        Toast.makeText(PasswordActivity.this, "Tree image clicked at position: " + position, Toast.LENGTH_SHORT).show();
 
 
-             }
-         });
-         Animals.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-             @Override
-             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                 Toast.makeText(PasswordActivity.this, "Animals image clicked at position: " + position, Toast.LENGTH_SHORT).show();
+                    }
+                });
+                Color.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        Toast.makeText(PasswordActivity.this, "Color image clicked at position: " + position, Toast.LENGTH_SHORT).show();
 
 
-             }
-         });
-         Places.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-             @Override
-             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                 Toast.makeText(PasswordActivity.this, "Places image clicked at position: " + position, Toast.LENGTH_SHORT).show();
+                    }
+                });
+                dailyObjects.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        Toast.makeText(PasswordActivity.this, "Daily Object image clicked at position: " + position, Toast.LENGTH_SHORT).show();
 
 
-             }
-         });
-         Vehicle.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-             @Override
-             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                 Toast.makeText(PasswordActivity.this, "Vehicle image clicked at position: " + position, Toast.LENGTH_SHORT).show();
+                    }
+                });
+                Animals.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        Toast.makeText(PasswordActivity.this, "Animals image clicked at position: " + position, Toast.LENGTH_SHORT).show();
 
 
-             }
-         });
+                    }
+                });
+                Places.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        Toast.makeText(PasswordActivity.this, "Places image clicked at position: " + position, Toast.LENGTH_SHORT).show();
 
 
+                    }
+                });
+                Vehicle.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        Toast.makeText(PasswordActivity.this, "Vehicle image clicked at position: " + position, Toast.LENGTH_SHORT).show();
+
+
+                    }
+                });
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.d("PasswordActivity", "Error getting documents: ", e);
+            }
+        });
 
          ImageView backButton = findViewById(R.id.backButton);
          backButton.setOnClickListener(new View.OnClickListener() {
